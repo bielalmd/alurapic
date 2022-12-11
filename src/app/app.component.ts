f0f65d0f77424e9fb46992cf8f8dbdc2d98852ba
bielalmd <austbiel@gmail.com>
2022-12-11 09:30:38 -0300

    componente de padrao inicial do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html', 
+  styleUrls: ['./app.component.css'] 
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent {}
+// em ts e js precisamos de this para acessar a propriedade de uma classe
+// em classe abstrata eu nao posso usar o operador new
